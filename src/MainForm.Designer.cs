7c913012e7f10424825199cff646f57e42ab7ffa
maifeeulasad <maifeeulasad@gmail.com>
2019-03-15 15:40:11 +0600

    added url/search bar border
    ui enhancement

--- src/MainForm.Designer.cs
+++ src/MainForm.Designer.cs
@@ -43,9 +43,8 @@
             this.PanelToolbar = new System.Windows.Forms.Panel();
             this.TabPages = new FarsiLibrary.Win.FATabStrip();
             this.tabStrip1 = new FarsiLibrary.Win.FATabStripItem();
-            this.loadingProgressBar = new System.Windows.Forms.ProgressBar();
-            this.tabStripAdd = new FarsiLibrary.Win.FATabStripItem();
             this.PanelStatus = new System.Windows.Forms.Panel();
+            this.tabStripAdd = new FarsiLibrary.Win.FATabStripItem();
             this.PanelSearch = new System.Windows.Forms.Panel();
             this.BtnNextSearch = new System.Windows.Forms.Button();
             this.BtnPrevSearch = new System.Windows.Forms.Button();
@@ -55,7 +54,6 @@
             this.PanelToolbar.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.TabPages)).BeginInit();
             this.TabPages.SuspendLayout();
-            this.tabStrip1.SuspendLayout();
             this.PanelSearch.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -160,12 +158,12 @@
             // 
             this.TxtURL.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.TxtURL.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TxtURL.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.TxtURL.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TxtURL.Location = new System.Drawing.Point(60, 5);
             this.TxtURL.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.TxtURL.Name = "TxtURL";
-            this.TxtURL.Size = new System.Drawing.Size(812, 22);
+            this.TxtURL.Size = new System.Drawing.Size(812, 29);
             this.TxtURL.TabIndex = 5;
             this.TxtURL.Click += new System.EventHandler(this.txtUrl_Click);
             this.TxtURL.TextChanged += new System.EventHandler(this.txtUrl_TextChanged);
@@ -207,7 +205,6 @@
             // 
             // tabStrip1
             // 
-            this.tabStrip1.Controls.Add(this.loadingProgressBar);
             this.tabStrip1.IsDrawn = true;
             this.tabStrip1.Name = "tabStrip1";
             this.tabStrip1.Selected = true;
@@ -215,14 +212,13 @@
             this.tabStrip1.TabIndex = 0;
             this.tabStrip1.Title = "Loading...";
             // 
-            // loadingProgressBar
+            // PanelStatus
             // 
-            this.loadingProgressBar.Location = new System.Drawing.Point(-1, 582);
-            this.loadingProgressBar.Name = "loadingProgressBar";
-            this.loadingProgressBar.Size = new System.Drawing.Size(PanelStatus.Width, 10);
-           
-            this.loadingProgressBar.Value = 50;
-            this.loadingProgressBar.Click += new System.EventHandler(this.loadingProgressBar_Click);
+            this.PanelStatus.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.PanelStatus.Location = new System.Drawing.Point(0, 651);
+            this.PanelStatus.Name = "PanelStatus";
+            this.PanelStatus.Size = new System.Drawing.Size(934, 20);
+            this.PanelStatus.TabIndex = 8;
             // 
             // tabStripAdd
             // 
@@ -232,14 +228,6 @@
             this.tabStripAdd.Size = new System.Drawing.Size(931, 601);
             this.tabStripAdd.TabIndex = 1;
             this.tabStripAdd.Title = "+";
-            // 
-            // PanelStatus
-            // 
-            this.PanelStatus.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.PanelStatus.Location = new System.Drawing.Point(0, 651);
-            this.PanelStatus.Name = "PanelStatus";
-            this.PanelStatus.Size = new System.Drawing.Size(934, 20);
-            this.PanelStatus.TabIndex = 8;
             // 
             // PanelSearch
             // 
@@ -336,7 +324,6 @@
             this.PanelToolbar.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.TabPages)).EndInit();
             this.TabPages.ResumeLayout(false);
-            this.tabStrip1.ResumeLayout(false);
             this.PanelSearch.ResumeLayout(false);
             this.PanelSearch.PerformLayout();
             this.ResumeLayout(false);
@@ -365,7 +352,6 @@
 		private System.Windows.Forms.Button BtnCloseSearch;
 		private System.Windows.Forms.Button BtnPrevSearch;
 		private System.Windows.Forms.Button BtnNextSearch;
-        private System.Windows.Forms.ProgressBar loadingProgressBar;
     }
 }
 
